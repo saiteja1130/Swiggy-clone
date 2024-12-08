e450404b2f2d617a511f0d1c6299dad3fcd5cf76
saiteja1130 <saitejanetha1130@gmail.com>
2024-12-08 12:19:14 +0530

    Updated footer swiggy.js

--- swiggy.js
+++ swiggy.js
@@ -21,7 +21,31 @@ locationPlace.addEventListener("click",()=>{
 });
 placeSearchicon.addEventListener("click",()=>{
     placeSearch.style.transform="translateX(-101%)"
-})
+});
+
+let searchPage=document.querySelector(".search-page");
+let searchOption=document.querySelector(".search-option");
+let navBar=document.querySelector(".navbar-js");
+let foodItem=document.querySelector(".food-items-js");
+let topResturent=document.querySelector(".top-restarents-js");
+let closeSearch=document.querySelector(".close-icon");
+let topResturentWithDelivery=document.querySelector(".top-restarents-onine-food-delivery-js");
+
+searchPage.addEventListener("click" , ()=>{
+    searchOption.style.display="block";
+    foodItem.style.display="none";
+    topResturent.style.display="none";
+    topResturentWithDelivery.style.display="none";
+});
+
+closeSearch.addEventListener("click" , ()=>{
+    searchOption.style.display="none";
+    foodItem.style.display="block";
+    topResturent.style.display="block";
+    topResturentWithDelivery.style.display="block";
+});
+
+
 
 
 
